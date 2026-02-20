7b20575c13feb2f4e73a1970cd9d389c7281d995
Swarup <swarupkst@gmail.com>
2026-02-21 00:55:43 +0600

    addEventListener

---
+++ addEventListener/script.js
@@ -0,0 +1,9 @@
+document.getElementById('red').addEventListener('click',function makeRed(){
+    document.body.style.backgroundColor='red'
+    document.querySelector('h1').style.color='white'
+})
+
+document.getElementById('blue').addEventListener('click', function makeBlue(){
+    document.body.style.backgroundColor='blue'
+    document.querySelector('h1').style.color='white'
+})
